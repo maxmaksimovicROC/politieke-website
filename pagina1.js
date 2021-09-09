82e44dfc59129e71132bda77783de4c3b223facc
max maksimovic <maxmaksimovic@student.rocva.nl>
2021-09-09 20:01:30 +0200

    carosel works

--- pagina1.js
+++ pagina1.js
@@ -1,42 +1,43 @@
 let slidePosition = 0;
-const slides = document.getElementsByClassName('carousel_item');
-const totalSlides = slides.length
+const slides = document.getElementsByClassName('carousel__item');
+const totalSlides = slides.length;
 
 document.
- getElementById('carousel_button--next')
- .addEventListener("click", function(){
-     moveToNextSlide();
- });
+  getElementById('carousel__button--next')
+  .addEventListener("click", function() {
+    moveToNextSlide();
+  });
+document.
+  getElementById('carousel__button--prev')
+  .addEventListener("click", function() {
+    moveToPrevSlide();
+  });
 
- document.
- getElementById('carousel_button--prev')
- .addEventListener("click", function(){
-     moveToPrevSlide();
- }); 
- function updateSlideposition(){
-     for (let slide of slides){
-         slide.classList.remove('carousel_item--visible');
-         slide.classList.add('carousel_item--hidden');
-     }
-     slides[slidePosition].classList.add('carousel_item--visible');
- }
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove('carousel__item--visible');
+    slide.classList.add('carousel__item--hidden');
+  }
 
- function moveToNextSlide(){
-     if (slidePosition === totalSlides - 1){
-         slidePosition = 0;
-     } else {
-         slidePosition++;
-     }
-     updateSlidePosition();
- }
+  slides[slidePosition].classList.add('carousel__item--visible');
+}
 
- function moveToPrevSlide(){
-    if(slidePosition === 0){
-        slidePosition = totalSlides -1;
-    } else{
-        slidePosition--;
-    }
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
 
-    updateSlideposition();
- }
- 
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+
+  updateSlidePosition();
+}

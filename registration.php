f04e87866e39714a3357754cad73e8ba43535070
max maksimovic <maxmaksimovic@student.rocva.nl>
2021-11-14 15:26:11 +0100

    reg form error

--- registration.php
+++ registration.php
@@ -1,18 +1,44 @@
+ <?php
+require_once('config.php');
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
   <title>Registration from</title>
   <link rel="stylesheet" href="boot/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/registration.css">
 </head>
 <body>
   <div>
     <?php
     if(isset($_POST['create'])){
-      echo 'account gemaakt.';
+      $firstname    = $_POST['firstname'];
+      $lastname     = $_POST['lastname'];
+      $Email        = $_POST['Email'];
+      $phonenumber  = $_POST['phonenumber'];
+      $password     = $_POST['password'];
+
+
+      $sql = "INSERT INTO users (firstname, lastname, Email, phonenumber, password) VALUES(?,?,?,?,?)";
+       $stminsert = $db->prepare($sql);
+       $result = $stminsert->execute([$firstname, $lastname, $Email, $phonenumber, $password]);    
+        if ($result) {
+          echo 'Account opgeslagen u kunt nu inloggen.';
+        }else{
+          echo 'Er ging iets fout probeer het opnieeuw.';
+        }
     }
     ?>
 
   </div>
+    <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="standpunt.php">Standpunten</a></li>
+            <li><a href="politici.php">Politici</a></li>
+            <li style="float:right"><a class="active" href="login.php">Login</a></li>
+            
+    </ul>
   <div>
     <form action="registration.php" method="post">
         <div class="container">
@@ -27,19 +53,19 @@
               <input class="form-control" type="text" name="firstname" required>
 
               <label for="lastname"><b>Achternaam</b></label>
-              <input class="form-control" input type="text" name="lastname" required>
+              <input class="form-control" type="text" name="lastname" required>
 
               <label for="Email"><b>Email</b></label>
-              <input class="form-control" input type="email" name="Email" required>
+              <input class="form-control" type="email" name="Email" required>
 
-              <label for="telnum"><b>Telefoon Nummer</b></label>
-              <input class="form-control" input type="text" name="telnum" required>
+              <label for="phonenumber"><b>Telefoon Nummer</b></label>
+              <input class="form-control" type="text" name="phonenumber" required>
 
               <label for="password"><b>Wachtwoord</b></label>
-              <input class="form-control" input type="password" name="password" required>
+              <input class="form-control" type="password" name="password" required>
 
               <hr class="mb-3">
-              <input class="form-control" input type="submit" name="create" value="Sign Up">
+              <input class="btn btn-primary" type="submit" name="create" value="Sign Up">
               </div>
 
           </div>    
